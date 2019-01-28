013ceac13975e0de41468605f0cd00e487b607b5
Risman <rsmnznr@gmail.com>
2019-01-28 15:25:23 +0700

    updated

--- src/components/ActionsBar/ActionsBar.js
+++ src/components/ActionsBar/ActionsBar.js
@@ -1,2 +1,195 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-Link"
+import screenfull from "screenfull";
+import { connect } from "react-redux";
+import { Icon, IconButton, withStyles } from "@material-ui/core";
+
+import {
+	setNavigatorPosition,
+	setNavigatorShape,
+	setScrollTop,
+	setFontSizeIncrease,
+	setCategoryFilter
+} from "../../state/store";
+import { featuredNavigator, moveNavigatorAside } from "./../../utils/shared";
+import FontSetter from "./FontSetter";
+import CategoryFilter from "./CategoryFilter";
+
+const styles = theme => ({
+	actionBar: {
+		position: "absolute",
+		background: theme.bars.colors.background,
+		left: 0,
+		bottom: 0,
+		display: "flex",
+		padding: `0 ${theme.base.sizes.linesMargin}`,
+		justifyContent: "space-between",
+		height: `${theme.base.sizes.linesMargin}px`,
+		width: "100%",
+		"&::before": {
+			position: "absolute",
+			left: theme.base.sizes.linesMargin,
+			right: theme.base.sizes.linesMargin,
+			height: 0,
+			top: 0,
+			borderTop: `1px solid ${theme.base.colors.lines}`
+		},
+		[`@media (min-width: ${theme.mediaQueryTresholds.M}px)`]: {
+			padding: `0 calc(${theme.base.sizes.linesMargin} * 1.5)`
+		},
+		[`@media (min-width: ${theme.mediaQueryTresholds.L}px)`]: {
+			flexDirection: "column",
+			top: 0,
+			right: 0,
+			left: "auto",
+			height: "100%",
+			padding: `${theme.base.sizes.linesMargin} 0`,
+			width: `${theme.bars.sizes.actionBar}px`,
+			"$::before": {
+				top: theme.base.sizes.linesMargin,
+				bottom: theme.base.sizes.linesMargin,
+				left: 0,
+				right: "auto",
+				width: 0,
+				height: "auto",
+				borderLeft: `1px solid ${theme.base.colors.lines}`
+			}
+		}
+	},
+	group: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		[`@media (min-width: ${theme.mediaQueryTresholds.L}px)`]: {
+			flexDirection: "column"
+		}
+	},
+	button: {
+		color: theme.bars.colors.icon
+	}
+});
+
+class ActionsBar extends React.Component {
+	state = {
+		fullscreen: false
+	};
+
+	componentDidMount() {
+		if (screenfull.enabled) {
+			screenfull.on("change", () => {
+				this.setStae({
+					fullscreen: screenfull.isFullscreen
+				});
+			});
+		}
+	}
+
+	homeOnClick = featuredNavigator.bind(this);
+	searchOnClick = moveNavigatorAside.bind(this);
+
+	fullscreenOnClick = () => {
+		if (screenfull.enabled) {
+			screenfull.toggle();
+	}
+	};
+
+	arrowUpOnclock = () => {
+		this.props.setScrollTop(true);
+	};
+
+	fontSetterOnClick = val => {
+		this.props.setFontSizeIncrease(val);
+
+		if (typeof localStorage !== "undefined") {
+			localStorage.setItem("font-size-increase". val)
+		}
+	};
+
+	CategoryFilterOnClick = val => {
+		this.props.setCategoryFilter(val);
+	}
+
+	render() {
+		const { clasess, NavigatorPosition, navigatorShape, isWideScreen, categories } = this.props;
+
+		return (
+			<div className={classes.actionBar}>
+				<div className={classes.group}>
+					<IconButton
+						aria-label="Back to list"
+						onClick={this.homeOnClick}
+						title="Back to the list"
+						className={classes.button}
+					>
+						<Icon>home</Icon>
+					</IconButton>
+					{((isWideScreen && navigatorShape === "open") || navigatorPosition !== "is-aside") && (
+						<CategoryFilter categories={categories} filterCategory={this.CategoryFilterOnClick} />
+					)}
+					<IconButton
+						aria-label="Search"
+						onClick={this.searchOnClick}
+						component={Link}
+						data-shape="closed"
+						to="/search/"
+						title="search"
+						className={classes.button}
+					>
+						<Icon className={classes.button}>search</Icon>
+					</IconButton>
+				</div>
+				<div className={classes.group}>
+					{navigatorPosition == "is-aside" && <FontSetter increaseFont={this.fontSetterOnClick} />}
+					{screenfull.enabled && (
+						<IconButton
+							aria-label="Fullscreen"
+							onClick={this.fullscreenOnClick}
+							title="Fullscreen mode"
+							className={classes.button}
+						>
+							{this.state.fullscreen ? <Icon>fullscreen_exit</Icon> : <Icon>Fullscreen</Icon>}
+						</IconButton>
+					)}
+						<IconButton aria-label="Back to top" onClick={this.arrowUpOnclock} title="Scroll to top">
+							<Icon className={classes.button} />
+						</IconButton>
+				</div>
+			</div>
+		);
+	}
+}
+
+ActionsBar.propTypes = {
+	classes: PropTypes.object.isRequired,
+	navigatorPosition: PropTypes.string.isRequired,
+	navigatorShape: PropTypes.string.isRequired,
+	isWideScreen: PropTypes.bool.isRequired,
+	setScrollTop: PropTypes.func.isRequired,
+	setFontSizeIncrease: PropTypes.func.isRequired,
+	categories: PropTypes.array.isRequired,
+	setCategoryFilter: PropTypes.func.isRequired,
+	CategoryFilter: PropTypes.string.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		navigatorPosition: state.navigatorPosition,
+		navigatorShape: state.navigatorShape,
+		isWideScreen: state.isWideScreen,
+		CategoryFilter: state.CategoryFilter
+	};
+};
+
+const mapDispatchToProps = {
+	setNavigatorPosition,
+	setNavigatorShape,
+	setScrollTop,
+	setFontSizeIncrease,
+	setCategoryFilter
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withStyles(styles)(ActionsBar));
---
+++ src/components/ActionsBar/CategoryFilter.js
@@ -0,0 +1,111 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import {
+	MenuItem, MenuList, IconButton, Grow,
+	Paper, ClickAwayListener, withStyles,
+	Icon, Popper
+} from "@material-ui/core";
+
+const styles= theme => ({
+	// fontSizeSetter: {
+	// 	[`@media (min-width: ${theme.mediaQueryTresholds.M}px)`]: {}
+	// },
+	open: {
+		color: theme.bars.colors.icon
+	},
+	popperClose: {
+		pointerEvents: "none"
+	},
+	popper: {
+		zIndex: 1
+	}
+});
+
+class CategoryFilter extends React.Component {
+	state = {
+		anchorEl: null,
+		opem: false
+	};
+
+	ComponentWillUnmount() {
+		clearTimeout(this.timeout);
+	}
+
+	handleClick = event => {
+		const { currentTarget } = event;
+		this.setState(state => ({
+			anchorEl: currentTarget,
+			open: !state.open
+		}));
+	};
+
+	handleClose = () => {
+		if (!this.state.open) {
+			return;
+		}
+
+		this.timeout = setTimeout(() => {
+			this.setState({ open: false });
+		});
+	};
+
+	handleFiltering = e => {
+		const category = e.target.innerText.trim();
+		this.props.filterCategory(category);
+		this.handleClose();
+	};
+
+	render() {
+		const { classes, categories } = this.props;
+		const { open, anchorEl } = this.state;
+		const id = open ? 'simple-popper' : null;
+
+		return (
+			<nav classNames={classes.fontSizeSetter}>
+				<IconButton
+					aria-label="Filter by Category"
+					aria-hashpopup="true"
+					aria-describedby={id}
+					onClick={this.handleClick}
+					variant="contained"
+					title="Filter the list by category"
+					className="classes.open"
+				>
+					<Icon>FilterList</Icon>
+				</IconButton>
+				<Popper
+					id={id} open={open}
+					anchorEl={anchorEl}
+					transition
+          className={`${classNames({ [classes.popperClose]: !open })} ${classes.popper}`}
+				>
+				<ClickAwayListener onClickAway={this.handleClose}>
+					<Grow in={open} id="cat-menu-list" style={{ transforOrigin: "0 0 0"}}>
+						<Paper>
+							<MenuList role="menu">
+								<MenuItem key="all" onClick={this.handleFiltering}>
+									all posts
+								</MenuItem>
+								{categories.map(category => (
+									<MenuItem key={category} onClick={this.handleFiltering}>
+										{category}
+									</MenuItem>
+								))}
+							</MenuList>
+						</Paper>
+					</Grow>
+				</ClickAwayListener>
+				</Popper>
+			</nav>
+		);
+	}
+}
+
+CategoryFilter.propTypes = {
+	classes: PropTypes.object.isRequired,
+	categories: PropTypes.array.isRequired,
+	filterCategory: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(CategoryFilter);
